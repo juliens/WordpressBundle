77202f3d854e6da62615430ef4b7c524f47cbb18
Julien SALLEYRON <julien.salleyron@gmail.com>
2013-12-04 23:44:21 +0100

    Calcul automatique des chemins à l'activation de l'extension

--- sources/wordpress/sf2/sf2plugin.php
+++ sources/wordpress/sf2/sf2plugin.php
@@ -1,134 +1,178 @@
 <?php 
-    use Symfony\Component\HttpFoundation\Request;
-		/*
-		Plugin Name: Integration Symfony 2
-		Author: J. Salleyron
-		Version: 1.0
-		*/
+/*
+Plugin Name: Integration Symfony 2
+Author: J. Salleyron
+Version: 1.0
+ */
 
-    class Sf2Plugin {
-        private $container = null;
-        private $kernel = null;
-        private $in_sf2 = true;
+use Symfony\Component\HttpFoundation\Request;
 
-        private function loadSf2()
-        {
-                global $kernel;
-                    //@settings_fields('wp_symfony_settings');
-                    //@do_settings_fields('wp_symfony_settings');
-                    $path = (get_option('symfony2_path'));
-                    $env = (get_option('symfony2_env'));
-                    $url = (get_option('symfony2_url'));
-                    if ($path==null) {
-                        $path = __DIR__.'/../../../../';
-                        update_option('symfony2_path', $path);
-                    }
-                    if ($env==null) {
-                        $env = 'dev';
-                        update_option('symfony2_env', $env);
-                    }
+class Sf2Plugin {
+    private $container = null;
+    private $kernel = null;
+    private $in_sf2 = true;
 
-                if (!file_exists($path.'app/bootstrap.php.cache')) {
-                    add_action( 'admin_footer', array( $this, 'symfony2_warning' ) );
-                    return;
-                }
+    private function isValidSymfonyPath($dir)
+    {
+        return (file_exists($dir.'app/bootstrap.php.cache'));
+    }
 
+    private function calculatePath()
+    {
+        $dir = __DIR__;
+        if (($pos = strpos($dir, 'vendor'))!==false) {
+            //Lien symbolique dans les vendor
 
-                if ($kernel==null) {
-                    $loader = require_once $path.'app/bootstrap.php.cache';
-                    require_once  $path.'app/AppKernel.php';
-                    $kernel = new AppKernel($env, true);
-                    $kernel->loadClassCache();
-                    $kernel->boot();
-                    $this->kernel = $kernel;
-                    $this->container = $kernel->getContainer();
-                    $this->container->get('session')->set('test','test');
-                } else {
-                    $this->kernel = $kernel;
-                    $this->container = $kernel->getContainer();
-                }
-                if ($url!=null) {
-                    preg_match('%([^:]*):\/\/([^\/]*)(\/.*)%', $url, $matches);
-                    if (count($matches)==4) {
-                        $context = $this->container->get('router')->getContext();
-                        $context->setHost($matches[2]);
-                        $context->setScheme($matches[1]);
-                        $context->setBaseUrl($matches[3]);
-                    } else {
-                        add_action( 'admin_footer', array( $this, 'symfony2_url_warning' ) );
-
-                    }
-                }
-                $wp_loader = $this->container->get('wordpress.loader');
-                $wp_loader->load();
-                
-
-        }
-        public function symfony2_url_warning()
-        {
-            echo "<div id='message' class='error'>";
-            echo "La configuration de votre url de symfony2 est incorrect. Vous devez mettre quelque chose du style http://serveur/path/vers/app.php";
-            echo "</div>";
-        }
-        public function symfony2_warning()
-        {
-            echo "<div id='message' class='error'>";
-            echo "La configuration de votre path vers symfony2 est incorrect. Vous devez mettre quelque chose comme /home/moi/monsite/app/";
-            echo "</div>";
-        }
-        public function getContainer()
-        {
-            return $this->container;
-        }
-
-        public function __construct()
-        {
-            add_action( 'admin_menu', array ($this, 'menu_params') );
-            add_action('admin_init', array($this, 'admin_init'));
-
-            $this->loadSf2();
-
-        }
-
-        public function admin_init()
-        {
-            //add_management_page( 'Custom Permalinks', 'Custom Permalinks', 'manage_options', 'my-unique-identifier', 'custom_permalinks_options_page' );
-            if ($this->container!=null) {
-                $shortcodes = $this->getContainer()->get('wordpress.loader')->getShortcodes();
-                foreach ($shortcodes as $shortcode) {
-                    register_setting('wp_symfony_settings', 'shortcode_'.$shortcode->getName());
-                }
+            $dir = substr($dir, 0, $pos);
+            if (!$this->isValidSymfonyPath($dir)) {
+                return null;
             }
-            register_setting('wp_symfony_settings', 'symfony2_path');
-            register_setting('wp_symfony_settings', 'symfony2_url');
-            register_setting('wp_symfony_settings', 'symfony2_env');
-        }
-
-        public function menu_params()
-        {
-            add_options_page( 'Symfony2 configuration','Symfony2','manage_options','options_symfony2', array( $this, 'settings_symfony2' ) );
-            //add_menu_page('Symfony2 configuration', 'Symfony2', 'manage_options', 'symfony2_options', array($this, 'menu_params_page'), plugins_url( 'sf2/images/icon.png' ), 6 ); 
-        }
-
-        public function settings_symfony2()
-        {
-            include(dirname(__FILE__).'/settings.php');
-        }
-
-        public function menu_params_page()
-        {
-            if ($this->container!=null) {
-                $shortcodes = $this->getContainer()->get('wordpress.loader')->getShortcodes();
+            return $dir;
+        } elseif (($pos=strpos($dir, 'wordpress/wp-content'))!==false) {
+            $dir = substr($dir, 0, $pos);
+            if (!$this->isValidSymfonyPath($dir)) {
+                return null;
             }
-            include(dirname(__FILE__).'/settings.php');
-        }
-
-        public function get($id)
-        {
-            return $this->getContainer()->get($id);
+            return $dir;
         }
     }
 
+    private function calculateUrl()
+    {
+        $url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+        if (($pos = strpos($url, 'wordpress/wp-admin')) != false) {
+            $url = substr($url, 0, $pos).'web/app_dev.php';
+        }
+        return $url;
+    }
 
 
-    $sf2plugin = new Sf2Plugin();
+    private function loadSf2()
+    {
+        global $kernel;
+        //@settings_fields('wp_symfony_settings');
+        //@do_settings_fields('wp_symfony_settings');
+        $path = (get_option('symfony2_path'));
+        $env = (get_option('symfony2_env'));
+        $url = (get_option('symfony2_url'));
+
+        if (!$this->isValidSymfonyPath($path)) {
+            add_action( 'admin_footer', array( $this, 'symfony2_warning' ) );
+            return;
+        }
+
+
+        if ($kernel==null) {
+            $loader = require_once $path.'app/bootstrap.php.cache';
+            require_once  $path.'app/AppKernel.php';
+            $kernel = new AppKernel($env, true);
+            $kernel->loadClassCache();
+            $kernel->boot();
+            $this->kernel = $kernel;
+            $this->container = $kernel->getContainer();
+            $this->container->get('session')->set('test','test');
+        } else {
+            $this->kernel = $kernel;
+            $this->container = $kernel->getContainer();
+        }
+        if ($url!=null) {
+            preg_match('%([^:]*):\/\/([^\/]*)(\/.*)%', $url, $matches);
+            if (count($matches)==4) {
+                $context = $this->container->get('router')->getContext();
+                $context->setHost($matches[2]);
+                $context->setScheme($matches[1]);
+                $context->setBaseUrl($matches[3]);
+            } else {
+                add_action( 'admin_footer', array( $this, 'symfony2_url_warning' ) );
+
+            }
+        }
+        $wp_loader = $this->container->get('wordpress.loader');
+        $wp_loader->load();
+
+
+    }
+    public function symfony2_url_warning()
+    {
+        echo "<div id='message' class='error'>";
+        echo "La configuration de votre url de symfony2 est incorrect. Vous devez mettre quelque chose du style http://serveur/path/vers/app.php";
+        echo "</div>";
+    }
+    public function symfony2_warning()
+    {
+        echo "<div id='message' class='error'>";
+        echo "La configuration de votre path vers symfony2 est incorrect. Vous devez mettre quelque chose comme /home/moi/monsite/app/";
+        echo "</div>";
+    }
+    public function getContainer()
+    {
+        return $this->container;
+    }
+
+    public function __construct()
+    {
+        //register_activation_hook( __FILE__, array($this, 'activate'));
+        add_action('activate_sf2/sf2plugin.php', array($this, 'activate'));
+        add_action('admin_menu', array ($this, 'menu_params') );
+        add_action('admin_init', array($this, 'admin_init'));
+
+        $this->loadSf2();
+
+    }
+
+    public function activate()
+    {
+        if (get_option('symfony2_path')==null) {
+            update_option('symfony2_path', $this->calculatePath());
+        }
+        if (get_option('symfony2_env')==null) {
+            update_option('symfony2_env', 'dev');
+        }
+        if (get_option('symfony2_url')==null) {
+            update_option('symfony2_url', $this->calculateUrl());
+        }
+
+    }
+
+    public function admin_init()
+    {
+        //add_management_page( 'Custom Permalinks', 'Custom Permalinks', 'manage_options', 'my-unique-identifier', 'custom_permalinks_options_page' );
+        if ($this->container!=null) {
+            $shortcodes = $this->getContainer()->get('wordpress.loader')->getShortcodes();
+            foreach ($shortcodes as $shortcode) {
+                register_setting('wp_symfony_settings', 'shortcode_'.$shortcode->getName());
+            }
+        }
+        register_setting('wp_symfony_settings', 'symfony2_path');
+        register_setting('wp_symfony_settings', 'symfony2_url');
+        register_setting('wp_symfony_settings', 'symfony2_env');
+    }
+
+    public function menu_params()
+    {
+        add_options_page( 'Symfony2 configuration','Symfony2','manage_options','options_symfony2', array( $this, 'settings_symfony2' ) );
+        //add_menu_page('Symfony2 configuration', 'Symfony2', 'manage_options', 'symfony2_options', array($this, 'menu_params_page'), plugins_url( 'sf2/images/icon.png' ), 6 ); 
+    }
+
+    public function settings_symfony2()
+    {
+        include(dirname(__FILE__).'/settings.php');
+    }
+
+    public function menu_params_page()
+    {
+        if ($this->container!=null) {
+            $shortcodes = $this->getContainer()->get('wordpress.loader')->getShortcodes();
+        }
+        include(dirname(__FILE__).'/settings.php');
+    }
+
+    public function get($id)
+    {
+        return $this->getContainer()->get($id);
+    }
+}
+
+
+
+$sf2plugin = new Sf2Plugin();
